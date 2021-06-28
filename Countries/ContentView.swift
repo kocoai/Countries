258878071a2f1764f5374c8f49e0ed9bb6e070c0
kyen <kyen@pm.me>
2021-06-28 16:35:19 +0200

    Initial Commit

---
+++ Countries/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Countries
+//
+//  Created by Kien on 28/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Kien on 28/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
